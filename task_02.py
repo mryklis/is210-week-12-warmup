c4a6bdbc63ed0508f46ff3c2c0f9fd57fd2e263a
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-11-10 23:52:19 +0000

    task_02 complete

--- task_02.py
+++ task_02.py
@@ -10,5 +10,23 @@ class InvalidAgeError(Exception):
     pass
 
 def get_age(birthyear):
+    """Check f age is greater than or equal to 0
+
+    Attributes:
+        birthyear (int): year to compare
+
+    Returns:
+        age (int): current year minus birthyear
+        InvalidAgeError
+    Example:
+        >>> get_age(2099)
+        Traceback (most recent call last):
+        File "<stdin>", line 1, in <module>raise InvalidAgeError
+        InvalidAgeError
+    """
+
     age = datetime.datetime.now().year - birthyear
-    return age
+    if age >= 0:
+        return age
+    else:
+        raise InvalidAgeError
