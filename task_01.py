ff281ebaf0aec0a7b07c6fa80fbfed8bbea47684
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-11-10 23:42:45 +0000

    task_01 complete

--- task_01.py
+++ task_01.py
@@ -4,4 +4,24 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """key or index exception error
+
+    Attributes:
+        var1 (mixed): argument variable
+        var2 (mixed): argument variable
+    Returns:
+         LookupError: 'Warning: Your index/key doesn\'t exist.'
+    Example:
+        >>> simple_lookup([1,2], 4)
+        Warning: Your index/key doesn't exist.
+        [1,2]
+        >>> simple_lookup({}, 'banana')
+        Warning: Your index/key doesn't exist.
+        {}
+    """
+    
+    try:
+        return var1[var2]
+    except LookupError:
+        print 'Warning: Your index/key doesn\'t exist.'
+    return var1
